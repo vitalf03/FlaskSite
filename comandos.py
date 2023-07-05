e67b8d4d4e39a65d0fc59a10736513246defc919
Vital Fernando <135348523+vitalf03@users.noreply.github.com>
2023-07-05 15:37:01 -0300

    Add files via upload

---
+++ comandos.py
@@ -0,0 +1,7 @@
+#<h1>(no começo e </h1> no final) sinaliza o titulo do site
+#para passar um for é necessario utilizar : {% (seu for) %}, depois é necessario : {%endfor%
+# para passar uma variavel no html é necessario usar {{ (sua variavel) }}
+# <form action = "/(rota predefinida)" method ="post"> serve para passar informações
+#methods=['METODO',] utilizamos no @approute para dizer se a pagina recebe get ou post
+# session[''] = request.form['objeto referenciado'] serve para armazenar o login nos cookies do navegador
+#flash() 
